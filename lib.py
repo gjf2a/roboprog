5f2aeba44cff7bd45bbc15ecb77c595200b1ed49
Ferrer <ferrer@hendrix.edu>
2023-06-19 16:30:38 -0500

    Initial commit.

---
+++ lib.py
@@ -0,0 +1,33 @@
+class Condition:
+    def __init__(self, sensor, checker, op, threshold):
+        self.sensor = sensor
+        self.checker = checker
+        self.op = op
+        self.threshold = threshold
+
+    def met(self):
+        return op(checker(sensor), threshold)
+
+
+class Action:
+    def __init__(self, motors, powers):
+        self.motors = motors
+        self.powers = powers
+
+    def act(self):
+        for i in range(len(self.motors)):
+            self.motors[i].on(self.powers[i])
+
+
+class Table:
+    def __init__(self):
+        self.pairs = []
+
+    def add_pair(self, condition, action):
+        self.pairs.append((condition, action))
+
+    def choose(self):
+        for (c, a) in self.pairs:
+            if c.met():
+                a.act()
+                break
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import time
+from ev3dev2.motor import Motor, OUTPUT_A, OUTPUT_B
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, UltrasonicSensor
+from ev3dev2.sound import Sound
+
+spkr = Sound()
+spkr.tone(440, 500)
+
+left = Motor(OUTPUT_A)
+right = Motor(OUTPUT_B)
+
+left_bump = TouchSensor(INPUT_1)
+right_bump = TouchSensor(INPUT_2)
+sonar = UltrasonicSensor(INPUT_3)
+
+start = time.time()
+
+while time.time() - start < 5:
+    if sonar.distance_centimeters < 30:
+        left.on(-25)
+        right.on(-25)
+    else:
+        if left_bump.is_pressed:
+            left.on(-25)
+        else:
+            left.on(25)
+        if right_bump.is_pressed:
+            right.on(-25)
+        else:
+            right.on(25)
+
+left.stop()
+right.stop()
