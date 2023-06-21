394ae76a0be178a99c20986400095c964a3bd95f
Ferrer <ferrer@hendrix.edu>
2023-06-21 07:37:44 -0500

    Detects touch sensors. Doesn't run motors.

--- lib.py
+++ lib.py
@@ -17,42 +17,84 @@ CONDITION_ASPECTS = 4
 def extract_port(port):
     return str(port).split('.')[1]
 
+
+def extract_num(numbed):
+    return int(numbed[1:]) - 1
+
+
 class Program:
-    def __init__(self, motors, sensors):
+    def __init__(self, ev3, motors, sensors):
         self.motors = {}
+        self.runners = {}
         self.motor_ports = []
         for i in range(len(motor_ports)):
             m = chr(ord('A') + i)
             if motors[i] == 'Motor':
-                self.motors[m] = Motor(motor_ports[i])
+                try:
+                    self.motors[m] = Motor(motor_ports[i])
+                    self.runners[m] = lambda v, m=m: self.motors[m].run(v)
+                except:
+                    ev3.screen.clear()
+                    ev3.screen.draw_text(0, 0, "Error")
+                    ev3.screen.draw_text(0, TEXT_HEIGHT, "Motor Port " + m)
+                    while True: pass
                 self.motor_ports.append(extract_port(motor_ports[i]))
             else:
                 self.motors[m] = None
 
         self.sensors = {}
+        self.readings = {}
         self.sensor_ports = []
         self.comparisons = {}
         self.thresholds = {}
         for i in range(len(sensor_ports)):
             s = 'S' + str(i + 1)
-            if sensors[i] == 'Touch':
-                self.sensors[s] = TouchSensor(sensor_ports[i])
-                self.sensor_ports.append(extract_port(sensor_ports[i]))
-                self.comparisons[s] = ['=']
-                self.thresholds[s] = ['1', '0']
-            elif sensors[i] == 'Sonar':
-                self.sensors[s] = UltrasonicSensor(sensor_ports[i])
-                self.sensor_ports.append(extract_port(sensor_ports[i]))
-                self.comparisons[s] = ['<', '>']
-                self.thresholds[s] = [str(i * 10) for i in range(1, 16)]
-            else:
-                self.sensors[s] = None
-                self.comparisons[s] = None
-                self.thresholds[s] = None
+            try:
+                if sensors[i] == 'Touch':
+                    self.sensors[s] = TouchSensor(sensor_ports[i])
+                    self.readings[s] = lambda s=s: 'Pressed' if self.sensors[s].pressed() else 'Open'
+                    self.sensor_ports.append(extract_port(sensor_ports[i]))
+                    self.comparisons[s] = ['==']
+                    self.thresholds[s] = ['Pressed', 'Open']
+                elif sensors[i] == 'Sonar':
+                    self.sensors[s] = UltrasonicSensor(sensor_ports[i])
+                    self.readings[s] = lambda s=s: str(self.sensors[s].distance())
+                    self.sensor_ports.append(extract_port(sensor_ports[i]))
+                    self.comparisons[s] = ['<', '>']
+                    self.thresholds[s] = [str(i * 10) for i in range(1, 16)]
+                else:
+                    self.sensors[s] = None
+                    self.comparisons[s] = None
+                    self.thresholds[s] = None
+            except:
+                ev3.screen.clear()
+                ev3.screen.draw_text(0, 0, "Error")
+                ev3.screen.draw_text(0, TEXT_HEIGHT, "Sensor Port " + s)
+                while True: pass
 
         self.conditions = {}
         self.actions = {}
-        self.tables = {}
+        self.rows = {}
+
+    def execute(self, ev3):
+        waitNonePressed(ev3)
+        while True:
+            pressed = ev3.buttons.pressed()
+            if len(pressed) > 0:
+                break
+            readings = self.check_all_conditions()
+            print(readings)
+            for name in sorted(self.rows):
+                row = self.rows[name]
+                if row.is_selected(readings):
+                    for action in row.actions(self):
+                        action.act(self)
+                    break
+
+        
+
+    def check_all_conditions(self):
+        return {p:v.check(self) for p,v in self.conditions.items()}
 
     def condition_option_list_and_choices(self, condition_name):
         if condition_name not in self.conditions:
@@ -67,14 +109,33 @@ class Program:
         ]
         return multi, c.choice_indices(self)
 
+    def action_choices(self, action_name):
+        if action_name not in self.actions:
+            p = self.motor_ports[0]
+            self.actions[action_name] = Action(action_name, p, '0')
+        return self.actions[action_name].choice_indices(self)
+
+    def row_choices(self, row_name):
+        if row_name not in self.rows:
+            self.rows[row_name] = Row(row_name, 'C1', 'False', 'None', 'None', 'None')
+        return self.rows[row_name].choice_indices(self)
+
     def change_condition_port(self, condition_name, new_port):
         c = self.conditions[condition_name]
         c.port = new_port
         c.op = self.comparisons[new_port][0]
         c.threshold = self.thresholds[new_port][0]
 
+    def change_action_port(self, action_name, new_port):
+        a = self.actions[action_name]
+        a.port = new_port
+        a.velocity = '0'
+
     def condition_options(self, port):
         return [self.comparisons[port], self.thresholds[port]]
+
+    def action_list(self):
+        return ['None'] + sorted(self.actions)
 
     def live_motors(self):
         return [name for name in self.motors if self.motors[name] is not None]
@@ -88,8 +149,8 @@ class Program:
     def num_actions(self):
         return len(self.actions)
 
-    def num_tables(self):
-        return len(self.tables)
+    def num_rows(self):
+        return len(self.rows)
 
 
 class Condition:
@@ -100,14 +161,68 @@ class Condition:
         self.threshold = threshold
 
     def __repr__(self):
-        return '[' + ','.join([self.name, self.port, self.op, self.threshold]) + ']'
+        return '[' + ','.join(["'" + s + "'" for s in [self.name, self.port, self.op, self.threshold]]) + ']'
 
     def choice_indices(self, program):
-        name_i = int(self.name[1:]) - 1
+        name_i = extract_num(self.name)
         port_i = program.sensor_ports.index(self.port)
         ops, ts = program.condition_options(self.port)
         op_i = ops.index(self.op)
         t_i = ts.index(self.threshold)
         return [name_i, port_i, op_i, t_i]
 
+    def check(self, program):
+        reading = program.readings[self.port]()
+        expr = '"' + reading + '" ' + self.op + ' "' + self.threshold + '"'
+        print(expr)
+        return self.port, reading, eval(expr)
 
+
+VELOCITIES = [str(180 * (i - 3)) for i in range(7)]
+
+
+class Action:
+    def __init__(self, name, port, velocity):
+        self.name = name
+        self.port = port
+        self.velocity = velocity
+
+    def __repr__(self):
+        return '[' + ','.join(["'" + s + "'" for s in [self.name, self.port, self.velocity]]) + ']'
+
+    def choice_indices(self, program):
+        name_i = extract_num(self.name)
+        port_i = program.motor_ports.index(self.port)
+        v_i = VELOCITIES.index(self.velocity)
+        return [name_i, port_i, v_i]
+
+    def act(self, program):
+        program.runners[self.port](int(self.velocity))
+
+
+class Row:
+    def __init__(self, name, condition, invert, action1, action2, action3):
+        self.name = name
+        self.condition = condition
+        self.invert = invert
+        self.action1 = action1
+        self.action2 = action2
+        self.action3 = action3
+
+    def __repr__(self):
+        return '[' + ','.join(["'" + s + "'" for s in [self.name, self.condition, self.invert, self.action2, self.action3]]) + ']'
+
+    def choice_indices(self, program):
+        name_i = extract_num(self.name)
+        condition_i = extract_num(self.condition)
+        invert_i = 0 if self.invert == "False" else 1
+        action_1 = program.action_list().index(self.action1)
+        action_2 = program.action_list().index(self.action2)
+        action_3 = program.action_list().index(self.action3)
+        return [name_i, condition_i, invert_i, action_1, action_2, action_3]
+
+    def is_selected(self, readings):
+        return self.condition in readings and readings[self.condition][2] == str(not eval(self.invert))
+
+    def actions(self, program):
+        return [program.actions[action] for action in [self.action1, self.action2, self.action3] if action != 'None']
--- main.py
+++ main.py
@@ -19,21 +19,30 @@ def main():
     ev3 = EV3Brick()
     ev3.speaker.beep()
 
-    motors = menus.menuManyOptions(ev3, motor_ports, motor_port_options)
-    sensors = menus.menuManyOptions(ev3, sensor_ports, sensor_port_options)
+    while True:
+        motors = menus.menuManyOptions(ev3, motor_ports, motor_port_options)
+        if any(motor != 'None' for motor in motors):
+            break
 
-    program = lib.Program(motors, sensors)
-    start = False
-    while not start:
-        choice = menus.menuShowAll(ev3, ['Conditions', 'Actions', 'Tables', 'Start'])
+    while True:
+        sensors = menus.menuManyOptions(ev3, sensor_ports, sensor_port_options)
+        if any(sensor != 'None' for sensor in sensors):
+            break
+
+    program = lib.Program(ev3, motors, sensors)
+    while True:
+        choice = menus.menuShowAll(ev3, ['Conditions', 'Actions', 'Rows', 'Start'])
         if choice == 0:
             condition_menu(ev3, program)
         elif choice == 1:
-            pass
+            action_menu(ev3, program)
         elif choice == 2:
-            pass
+            row_menu(ev3, program)
         elif choice == 3:
-            start = True
+            print(program.conditions)
+            print(program.actions)
+            print(program.rows)
+            program.execute(ev3)
 
 
 def condition_menu(ev3, program):
@@ -87,6 +96,105 @@ def condition_menu(ev3, program):
     lib.waitNonePressed(ev3)
 
 
+def action_menu(ev3, program):
+    menus.wait_until_clear(ev3)
+    rows = ['Name', 'Port', 'Deg/Sec']
+    names = ["A" + str(i + 1) for i in range(max(1, program.num_actions()))]
+    ports = program.live_motors()
+    name = 0
+    row = 0
+    down = False
+    refresh = True
+    multi_option_list = [names, ports, lib.VELOCITIES]
+    while True:
+        if refresh:
+            choices = program.action_choices(names[name])
+            menus.refreshMany(ev3, rows, multi_option_list, row, choices)
+            refresh = False
+
+        pressed = ev3.buttons.pressed()
+        if len(pressed) > 0:
+            if not down:
+                ev3.speaker.beep()
+                down = True
+                updated = None
+                if Button.CENTER in pressed:
+                    break
+                elif Button.UP in pressed:
+                    row = mod_dec(row, len(rows))
+                elif Button.DOWN in pressed:
+                    row = mod_inc(row, len(rows))
+                elif Button.LEFT in pressed:
+                    updated = mod_dec(choices[row], len(multi_option_list[row]))
+                elif Button.RIGHT in pressed:
+                    if row == 0 and choices[row] + 1 == len(names):
+                        names.append("A" + str(len(names) + 1))
+                    updated = mod_inc(choices[row], len(multi_option_list[row]))
+                if updated is not None:
+                    if row == 0:
+                        name = updated
+                    elif row == 1:
+                        program.change_action_port(names[name], multi_option_list[1][updated])
+                    elif row == 2:
+                        program.actions[names[name]].velocity = multi_option_list[2][updated]
+                refresh = True
+        else:
+            down = False
+
+    lib.waitNonePressed(ev3)
+        
+
+def row_menu(ev3, program):
+    menus.wait_until_clear(ev3)
+    rows = ['Name', 'Condition', 'Inverted', 'Action1', 'Action2', 'Action3']
+    names = ["R" + str(i + 1) for i in range(max(1, program.num_rows()))]
+    name = 0
+    row = 0
+    down = False
+    refresh = True
+    multi_option_list = [names, sorted(program.conditions), ["False", "True"], program.action_list(), program.action_list(), program.action_list()]
+    while True:
+        if refresh:
+            choices = program.row_choices(names[name])
+            menus.refreshMany(ev3, rows, multi_option_list, row, choices)
+            refresh = False
+
+        pressed = ev3.buttons.pressed()
+        if len(pressed) > 0:
+            if not down:
+                ev3.speaker.beep()
+                down = True
+                updated = None
+                if Button.CENTER in pressed:
+                    break
+                elif Button.UP in pressed:
+                    row = mod_dec(row, len(rows))
+                elif Button.DOWN in pressed:
+                    row = mod_inc(row, len(rows))
+                elif Button.LEFT in pressed:
+                    updated = mod_dec(choices[row], len(multi_option_list[row]))
+                elif Button.RIGHT in pressed:
+                    if row == 0 and choices[row] + 1 == len(names):
+                        names.append("R" + str(len(names) + 1))
+                    updated = mod_inc(choices[row], len(multi_option_list[row]))
+                if updated is not None:
+                    if row == 0:
+                        name = updated
+                    elif row == 1:
+                        program.rows[names[name]].condition = multi_option_list[1][updated]
+                    elif row == 2:
+                        program.rows[names[name]].invert = multi_option_list[2][updated]
+                    elif row == 3:
+                        program.rows[names[name]].action1 = multi_option_list[3][updated]
+                    elif row == 4:
+                        program.rows[names[name]].action2 = multi_option_list[4][updated]
+                    elif row == 5:
+                        program.rows[names[name]].action3 = multi_option_list[5][updated]
+                refresh = True
+        else:
+            down = False
+
+    lib.waitNonePressed(ev3)
 
 if __name__ == '__main__':
     main()
